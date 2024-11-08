48a57217e978240fa7bdf55cf1d0c6a4025f2a17
tiagoolindo2 <lu.nabuco.sil@gmail.com>
2024-11-08 15:07:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tiago',
+    'Quem e o Tiago?',
+    'E um menino mais lindo do mundo e gostoso que já vi'
+)
+
+criaCartao(
+    'Karina',
+    'Quem e prof. Karina?',
+    'E uma prof. bem legal e divertida e a mais linda da escola e namora um advogado'
+)
+
+criaCartao(
+    'Isaah',
+    'Quem e a Isaah?',
+    'uma menina inteligente e gosta de ouvir piseiro e gosta de girasol e ela que um cuelho'
+)
+
+criaCartao(
+    'Caio',
+    'Quem e o Caio?',
+    'Um homem emo e escravo e gosta de caixões e teia de aranha, brincos, preto, muito preto, preto dark, preto escuro, preto preto.'
+)
